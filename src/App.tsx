8d4458d1a0c1c26539dcb0bcc1def196e7012d40
Jordan Ricardo Gopie <jordangopie@live.com>
2023-04-26 19:58:32 -0400

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { AdviceGenerator } from './components/advice-generator/advice-generator.component'
+
+function App() {
+  return (
+    <>
+      <AdviceGenerator/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/advice-generator/advice-generator.component.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import "./advice-generator.component.css";
+
+export const AdviceGenerator = () => {
+    const [advice, setData] = useState({"slip": {"id": 0, "advice": "Rolling the Dice"}});
+    let mobileDivider =  new URL("/pattern-divider-mobile.svg", import.meta.url).href;
+    let desktopDivider = new URL("/pattern-divider-desktop.svg", import.meta.url).href;
+    let diceIcon = new URL("/icon-dice.svg", import.meta.url).href;
+    const [displayImage, setDisplayImage] = useState(window.outerWidth > 375 ? desktopDivider : mobileDivider);
+    
+    //Fetches first advice slip, and handles the window resizing.
+
+    const changeImage = () => {
+        setDisplayImage(window.outerWidth > 375 ? desktopDivider : mobileDivider);
+    }
+
+    useEffect(()=> {
+        fetch("https://api.adviceslip.com/advice")
+        .then(response => response.json())
+        .then(data => setData(data));
+
+        window.addEventListener('resize', changeImage);
+        return () => {
+            window.removeEventListener('resize', changeImage);
+        }
+    }, []);
+
+    return (
+        <div className="main-body flex">
+            <div className="content-container flex">
+                <p>Advice #{advice.slip.id}</p>
+                <p>"{advice.slip.advice}"</p>
+            </div>
+            <div className="image-container flex">
+                <img className="divider" src={displayImage}/>
+                <img className="dice" src={diceIcon}/>
+            </div>
+        </div>
+    );
+}
